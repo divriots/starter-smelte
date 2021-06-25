24d47168c854553d179ec79703bb4abf8aa3654a
m4dz <code@m4dz.net>
2021-06-25 18:12:54 +0200

    Add theme-switch component (#1)

---
+++ theme-switch/src/index.js
@@ -0,0 +1,2 @@
+import ThemeSwitch from './ThemeSwitch.svelte';
+export { ThemeSwitch };
---
+++ theme-switch/stories/index.stories.js
@@ -0,0 +1,6 @@
+import '~/smelte/src/smelte.css';
+import '~/smelte/src/gfonts.css';
+
+import { ThemeSwitch } from '../';
+
+export const storyDefault = () => ThemeSwitch;
